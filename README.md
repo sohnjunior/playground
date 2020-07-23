a6e95d906390805be5ea393128f66af6192deb49
handhand <sohnjunior@naver.com>
2020-07-23 13:23:07 +0900

    CSS 수정

--- README.md
+++ README.md
@@ -6,11 +6,19 @@
 
 ### 제로초 - 인프런 무료 강의 실습
 
+
+
 ## 직접 만들어보는 간단한 React 앱
 
-### Todo 관리 앱
+### Todo 앱
+* React Hook (useState, useRef, useEffect) 활용
+* Material UI
 
 ### (예정) 오목 게임
+* useReducer, Context API 활용
+* 
 
-### (예정) Redux 및 React-router 실습
+
+### (예정) 게시판
+* Redux 및 React-router 활용
 
--- react-todo/src/components/Todos.jsx
+++ react-todo/src/components/Todos.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import Todo from './Todo';
+import '../App.css';
 
 
 const Todos = ({ todoList, removeTodo, updateTodo }) => {
   return (
-    <table>
-      <tbody>
-        {todoList.map((todo, i) => {
-          return (
-            <tr key={i + todo}>
-              <Todo index={i} 
-                    item={todo} 
-                    removeTodo={removeTodo}
-                    updateTodo={updateTodo} />
-            </tr>
-          );
-        })}
-      </tbody>
+    <table className="table">
+      {todoList.map((todo, i) => {
+        return (
+          <tr key={i + todo}>
+            <Todo index={i} 
+                  item={todo} 
+                  removeTodo={removeTodo}
+                  updateTodo={updateTodo} />
+          </tr>
+        );
+      })}
     </table>
   );
 };
